8abef9f39398b66b945af4395aa2f3c5e6d0337d
liuzhic <liuzhic85@163.com>
2014-04-02 10:52:52 +0800

    重新提交
    twinkelmodel重新提交

--- CesiumAPI/api.twinkleModel.js
+++ CesiumAPI/api.twinkleModel.js
@@ -9,16 +9,11 @@ define(function(){
     if(id) {
       require(['CesiumAPI/api.idManager.js'], function() {
         var ary = window.idManager.getByID(id);
-        console.log(ary[0]);
         if(ary.length > 0) {
           var obj = ary[0];
           var timeId = setInterval(function() {
-            console.log(obj.show);
-            console.log(typeof(options.twinkle));
-             if(options.twinkle=="true") {
-               
+             if(options.twinkle=="true") {               
               if(obj.show==true)
-
                 obj.show=false;
               else if(obj.show==false)
                 obj.show=true;
